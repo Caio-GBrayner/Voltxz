83be254acc59d94ddf7f9790be5cd2b10c4c69f7
Francisco Primo <assisbsprimo@gmail.com>
2025-05-02 22:29:10 -0300

    add dto

--- back-end/src/modules/users/controllers/users.controller.ts
+++ back-end/src/modules/users/controllers/users.controller.ts
@@ -1,3 +1,4 @@
+import { UpdateUserDto } from '../dto/update-user.dto';
 import {
   Controller,
   Post,
@@ -12,7 +13,6 @@ import {
 import { UserService } from 'src/modules/users/services/users.service';
 import { User as UserModel } from 'generated/prisma';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { UpdateUserDto } from '../dto/update-user.dto'; 
 
 @Controller('api/users')
 export class UserController {
@@ -32,7 +32,7 @@ export class UserController {
 
   @Get(':id')
   async getUserById(
-    @Param('id') id: string,  
+    @Param('id', ParseIntPipe) id: number,  
   ): Promise<UserModel | null> {
     return this.userService.user({ id });
   }
@@ -50,8 +50,8 @@ export class UserController {
 
   @Put(':id')
   async updateUser(
-    @Param('id') id: string,  
-    @Body() updateUserDto: UpdateUserDto,
+    @Param('id', ParseIntPipe) id: number,  
+    @Body() updateUserDto: UpdateUserDto,  
   ): Promise<UserModel> {
     return this.userService.updateUser({
       where: { id },
@@ -60,7 +60,7 @@ export class UserController {
   }
 
   @Delete(':id')
-  async deleteUser(@Param('id') id: string): Promise<UserModel> {  
+  async deleteUser(@Param('id', ParseIntPipe) id: number): Promise<UserModel> {  
     return this.userService.deleteUser({ id });
   }
 }
