904db51550b50807bff841656af065ecc7652c04
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-05-06 14:45:48 -0300

    response-user.dto.ts

---
+++ back-end/src/modules/users/dto/response-user.dto.ts
@@ -0,0 +1,23 @@
+import { Exclude, Expose } from 'class-transformer';
+import { UserType } from 'generated/prisma';
+
+@Exclude()
+export class ResponseUserDto {
+  @Expose()
+  id!: string;
+  
+  @Expose()
+  name?: string;
+  
+  @Expose()
+  email!: string;
+  
+  @Expose()
+  user_type!: UserType;
+  
+  @Expose()
+  phone?: string;
+  
+  @Expose()
+  created_at?: Date;
+}
