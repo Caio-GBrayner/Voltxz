fc8044347579a5ecb3558fcdcbe30a54511a1d48
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-05-23 17:40:23 -0300

    feat(dtos) Create Company Upadate/Create:
    - Create -> CreateComapanyDto
    - Create -> UpdateCompanyDto -> @IsOptinal -> company_name & document_id

---
+++ back-end/src/modules/companies/dto/create-company.dto.ts
@@ -0,0 +1,18 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateCompanyDto {
+    @IsUUID()
+    user_id!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    document_id!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    company_name!: string;
+}
---
+++ back-end/src/modules/companies/dto/update-company.dto.ts
@@ -0,0 +1,17 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class UpdateCompanyDto {
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    document_id?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()   
+    company_name?: string;
+}
