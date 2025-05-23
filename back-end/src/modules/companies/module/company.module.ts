2959c5b5bd0414fd5861f5bd7a50a88fb16446b2
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-05-23 20:26:03 -0300

    feat(Module) Created Module:
    - Created -> Company Moule

---
+++ back-end/src/modules/companies/module/company.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { CompanyService } from "src/modules/companies/services/company.service";
+import { CompanyController } from "src/modules/companies/controllers/company.controller";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Module({
+  controllers: [CompanyController],
+  providers: [CompanyService, PrismaService],
+})
+export class CompanyModule {}
