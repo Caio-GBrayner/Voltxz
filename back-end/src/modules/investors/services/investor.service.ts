61bfafcf0dffe8e4d9fd50e9b91b68b3791a7ded
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-05-23 20:59:54 -0300

    refactor(service) class InvestorServie:
    - InvestorsService -> InvestorService

---
+++ back-end/src/modules/investors/services/investor.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateInvestorDto } from "src/modules/investors/dto/create-investor.dto";
+import { UpdateInvestorDto } from "src/modules/investors/dto/update-investor.dto";
+
+@Injectable()
+export class InvestorService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createInvestorDto: CreateInvestorDto) {
+    return this.prisma.investors.create({
+        data: createInvestorDto,
+    });
+  }
+    async findAll() {
+        return this.prisma.investors.findMany();
+    }
+
+    async findOne(id: string) {
+        return this.prisma.investors.findUnique({
+            where: { id },
+        });
+    }
+
+    async update(id: string, updateInvestorDto: UpdateInvestorDto) {
+        return this.prisma.investors.update({
+            where: { id },
+            data: updateInvestorDto,
+        });
+    }
+
+    async remove(id: string) {
+        return this.prisma.investors.delete({
+            where: { id },
+        });
+    }
+}
