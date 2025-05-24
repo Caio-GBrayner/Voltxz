96f62b6da79d05dd2efa1645c474cb22e1d108a9
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-05-23 21:01:10 -0300

    feat(Controller) Created InvestorCotroller:
    Created -> routes

---
+++ back-end/src/modules/investors/controllers/investor.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { InvestorService } from "../services/investor.service";
+import { CreateInvestorDto } from "../dto/create-investor.dto";
+import { UpdateInvestorDto } from "../dto/update-investor.dto";
+
+
+@Controller("/api/investors")
+export class InvestorsController {
+  constructor(private readonly investorService: InvestorService) {}
+
+  @Get()
+    async findAll() {
+        return this.investorService.findAll();
+    }
+
+    @Get(":id")
+    async findOne(@Param("id") id: string) {
+        return this.investorService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() createInvestorDto: CreateInvestorDto) {
+        return this.investorService.create(createInvestorDto);
+    }
+
+    @Patch(":id")
+    async update(@Param("id") id: string, @Body() updateInvestorDto: UpdateInvestorDto) {
+        return this.investorService.update(id, updateInvestorDto);
+    }
+
+    @Delete(":id")
+    async remove(@Param("id") id: string) {
+        return this.investorService.remove(id);
+    }
+    
+}
