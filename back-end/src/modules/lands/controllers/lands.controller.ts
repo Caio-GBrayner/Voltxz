cdb6f576a1f04dedee9198559add0e12c8a7fc82
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-05-30 14:24:56 -0300

    fix(create) service, controller dto:
    land_owner_id

--- back-end/src/modules/lands/controllers/lands.controller.ts
+++ back-end/src/modules/lands/controllers/lands.controller.ts
@@ -12,15 +12,20 @@ import { LandsService } from 'src/modules/lands/services/lands.service';
 import { CreateLandDto } from 'src/modules/lands/dto/create-land.dto';
 import { UpdateLandDto } from 'src/modules/lands/dto/update-land.dto';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { UserId } from 'src/decorators/current-user.decorator';
 
 @Controller('api/lands')
+@UseGuards(JwtAuthGuard)
 export class LandsController {
   constructor(private landsService: LandsService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
-  create(@Body() createLandDto: CreateLandDto) {
-    return this.landsService.create(createLandDto);
+  create(@Body() createLandDto: CreateLandDto, @UserId() userId: string) {
+    if (!userId) {
+      throw new Error('User ID is required to create a land.');
+    }
+
+    return this.landsService.create(createLandDto, userId);
   }
 
   @Get('available')
@@ -29,24 +34,20 @@ export class LandsController {
   }
 
   @Patch(':id')
-  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateLandDto: UpdateLandDto) {
     return this.landsService.update(id, updateLandDto);
   }
 
   @Get(':id')
-  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.landsService.findOne(id);
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.landsService.findAll();
   }
   @Delete(':id')
-  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.landsService.remove(id);
   }
